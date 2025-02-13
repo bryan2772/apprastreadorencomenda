b53106eb041647f4e9cc93e9f44325573328918f
bryan2772 <ryanpassosmg@gmail.com>
2025-02-12 23:23:41 -0300

    appbar sanduiche

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -46,7 +46,71 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Minhas Encomendas')),
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+        title: Text('Minhas Encomendas'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(const SnackBar(content: Text('notificaçoes ativas')));
+            },
+          ), 
+        ],  
+      ),
+  
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Color.fromARGB(255, 176, 163, 207)),
+              child: Text('Menu'),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Configuraçoes'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('arquivados'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+
       body: _encomendas.isEmpty
           ? Center(child: Text('Nenhuma encomenda cadastrada.'))
           : ListView.builder(
@@ -74,8 +138,11 @@ class HomeScreenState extends State<HomeScreen> {
             ),
       floatingActionButton: FloatingActionButton(
         onPressed: _adicionarEncomenda,
+        //backgroundColor: Colors.red,
+        shape: CircleBorder(),
         child: Icon(Icons.add),
       ),
+      
     );
   }
 }
